dc4a12d5402f9c286f5c480f9f86c10dc09b3caf
Tim Nieradzik <tim@sparse.tech>
2018-03-13 11:37:55 +0100

    App: Use decodeJs() instead of decode()

--- js/src/main/scala/example/App.scala
+++ js/src/main/scala/example/App.scala
@@ -1,28 +1,31 @@
 package example
 
-import example.page.Page
-import example.protocol.Request
 import org.scalajs.dom
 import org.scalajs.dom.ext.Ajax
+
 import pine._
 import pine.dom._
+
 import trail._
 
-import scala.concurrent.Future
 import scala.scalajs.js
-import scala.scalajs.js.JSON
+import scala.concurrent.Future
 import scala.scalajs.js.annotation.JSGlobalScope
 import scala.concurrent.ExecutionContext.Implicits.global
-import io.circe._
+
 import io.circe.parser._
 import io.circe.syntax._
+import io.circe.scalajs._
 import io.circe.generic.auto._
+
+import example.page.Page
+import example.protocol.Request
 
 @js.native
 @JSGlobalScope
 object JsGlobals extends js.Object {
   val templates: js.Dictionary[String] = js.native
-  val page     : js.Dictionary[js.Any] = js.native
+  val page     : js.Any = js.native
 }
 
 object Template {
@@ -61,13 +64,8 @@ object App {
     dom.document.body.scrollTop = 0
   }
 
-  def serverPage()(implicit decoder: Decoder[Page]): Page = {
-    val json = JSON.stringify(JsGlobals.page)
-    decode[Page](json).right.get
-  }
-
   def main(args: Array[String]): Unit = {
-    Window.load     := Manage.page(serverPage())
+    Window.load     := Manage.page(decodeJs[Page](JsGlobals.page).right.get)
     Window.popState := render(dom.window.location.href)
     Window.click    := { e =>
       // The user may click a child within <a>, traverse parents too
